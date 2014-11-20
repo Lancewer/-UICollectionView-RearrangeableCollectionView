2ed310432bccc1fba9f52b115cc79f747fc2dd43
Lancewer <lancewer@gmail.com>
2014-11-20 18:09:19 +0800

    add an initial view

---
+++ [UICollectionView]RearrangeableCollectionView/InitViewController.m
@@ -0,0 +1,41 @@
+//
+//  InitViewController.m
+//  [UICollectionView]RearrangeableCollectionView
+//
+//  Created by Lawrence on 11/20/14.
+//  Copyright (c) 2014 Lawrence. All rights reserved.
+//
+
+#import "InitViewController.h"
+
+@interface InitViewController ()
+
+@end
+
+@implementation InitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+-(IBAction) finishConfigure:(UIStoryboardSegue *)segue{
+    
+}
+
+@end
